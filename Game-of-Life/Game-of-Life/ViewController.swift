cbd152325dd053056db24cf96f50cbf9e86ed8a8
Emil Iliev <emil_iliev1994@abv.bg>
2017-12-05 07:55:56 +0200

    Added initial files

---
+++ Game-of-Life/Game-of-Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game-of-Life
+//
+//  Created by Emil Iliev on 12/5/17.
+//  Copyright © 2017 Emil Iliev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
